6946f2718e88d1f1e4a28ffb5c8a3c6dcb1150e9
creewick <ilia_y@mail.ru>
2024-07-19 13:46:09 +0300

    Log unhandled exception to file

--- uWidgets/Program.cs
+++ uWidgets/Program.cs
@@ -1,5 +1,6 @@
 ﻿using Avalonia;
 using System;
+using System.IO;
 using uWidgets.Core;
 
 namespace uWidgets;
@@ -10,9 +11,18 @@ class Program
     // SynchronizationContext-reliant code before AppMain is called: things aren't initialized
     // yet and stuff might break.
     [STAThread]
-    public static void Main(string[] args) =>
-        BuildAvaloniaApp()
-            .StartWithClassicDesktopLifetime(args);
+    public static void Main(string[] args)
+    {
+        try
+        {
+            BuildAvaloniaApp()
+                .StartWithClassicDesktopLifetime(args);
+        }
+        catch (Exception e)
+        {
+            File.WriteAllText("crash_log.txt", $"{e.Message}{Environment.NewLine}{e.StackTrace}");
+        }
+    }
 
     // Avalonia configuration, don't remove; also used by visual designer.
     public static AppBuilder BuildAvaloniaApp()
