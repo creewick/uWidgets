290e665616040322e7c13934cfde5b631d02e616
creewick <ilia_y@mail.ru>
2024-07-20 16:11:46 +0300

    Dynamic font size in Calendar - Month

--- Widgets/Calendar/ViewModels/DayViewModel.cs
+++ Widgets/Calendar/ViewModels/DayViewModel.cs
@@ -1,14 +1,3 @@
-using Avalonia;
-using Avalonia.Media;
-
 namespace Calendar.ViewModels;
 
-public record DayViewModel(string? Day = null, bool IsWeekend = false, bool IsToday = false)
-{
-    public double Opacity => IsWeekend && !IsToday ? 0.5 : 1;
-
-    public SolidColorBrush Foreground => IsToday
-        ? new SolidColorBrush(Colors.White)
-        // TODO ResourceManager is empty here
-        : new SolidColorBrush((Color)Application.Current!.Resources["SystemBaseMediumColor"]!);
-}
+public record DayViewModel(string? Day = null, bool IsWeekend = false, bool IsToday = false);
