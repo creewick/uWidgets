de1573b48fe28b76c666074963087f323971e3d0
creewick <ilia_y@mail.ru>
2024-07-24 20:57:05 +0300

    refactor(Notes): Modify margin

--- Widgets/Notes/Views/Note.axaml.cs
+++ Widgets/Notes/Views/Note.axaml.cs
@@ -12,18 +12,19 @@ namespace Notes.Views;
 
 public partial class Note : UserControl
 {
-    private readonly NoteModel noteModel;
+    private NoteModel model;
     private readonly IWidgetLayoutProvider widgetLayoutProvider;
     
-    public Note(IWidgetLayoutProvider widgetLayoutProvider) : this(new NoteModel(Locale.Notes_Title), widgetLayoutProvider) {}
+    public Note(IWidgetLayoutProvider widgetLayoutProvider) 
+        : this(new NoteModel(Locale.Notes_Title), widgetLayoutProvider) {}
     
-    public Note(NoteModel noteModel, IWidgetLayoutProvider widgetLayoutProvider)
+    public Note(NoteModel model, IWidgetLayoutProvider widgetLayoutProvider)
     {
-        this.noteModel = noteModel;
+        this.model = model;
         this.widgetLayoutProvider = widgetLayoutProvider;
-        DataContext = new NoteViewModel(noteModel);
+        DataContext = new NoteViewModel(model);
         
-        PointerExited += OnPointerExited;
+        LostFocus += OnLostFocus;
         SizeChanged += OnSizeChanged;
         Unloaded += OnUnloaded;
         InitializeComponent();
@@ -32,32 +33,31 @@ public partial class Note : UserControl
     private void OnSizeChanged(object? sender, SizeChangedEventArgs e)
     {
         var width = VisualRoot?.ClientSize.Width ?? 0;
-        
         (DataContext as NoteViewModel)!.LineEnd = new Point(width, 0);
     }
 
     private void OnUnloaded(object? sender, RoutedEventArgs e)
     {
-        PointerExited -= OnPointerExited;
+        PointerExited -= OnLostFocus;
         SizeChanged -= OnSizeChanged;
     }
 
     private void UpdateContent(object? sender, RoutedEventArgs e)
     {
         var newText = (sender as TextBox)!.Text;
-
-        var newModel = noteModel with { Content = newText, Updated = DateTime.Now };
-        var newSettings = JsonSerializer.SerializeToElement(newModel);
-        var newLayout = widgetLayoutProvider.Get() with { Settings = newSettings };
-        
-        widgetLayoutProvider.Save(newLayout);
+        UpdateModel(model with { Content = newText, Updated = DateTime.Now });
     }
     
     private void UpdateTitle(object? sender, RoutedEventArgs e)
     {
         var newText = (sender as TextBox)!.Text;
+        UpdateModel(model with { Title = newText, Updated = DateTime.Now });
+    }
 
-        var newModel = noteModel with { Title = newText, Updated = DateTime.Now };
+    private void UpdateModel(NoteModel newModel)
+    {
+        model = newModel;
+        DataContext = new NoteViewModel(newModel);
         var newSettings = JsonSerializer.SerializeToElement(newModel);
         var newLayout = widgetLayoutProvider.Get() with { Settings = newSettings };
         
@@ -65,5 +65,5 @@ public partial class Note : UserControl
     }
 
     private void OverlayClick(object? sender, PointerPressedEventArgs e) => Overlay.IsVisible = false;
-    private void OnPointerExited(object? sender, PointerEventArgs e) => Overlay.IsVisible = true;
+    private void OnLostFocus(object? sender, RoutedEventArgs e) => Overlay.IsVisible = true;
 }
