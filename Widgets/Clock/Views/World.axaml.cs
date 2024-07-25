d1cdca076a961cfb90c434da7e55e3d744e6ea16
creewick <ilia_y@mail.ru>
2024-07-25 10:40:33 +0300

    feat(Clock): Wide view for World Clock widget

--- Widgets/Clock/Views/World.axaml.cs
+++ Widgets/Clock/Views/World.axaml.cs
@@ -1,4 +1,5 @@
 ﻿using Avalonia.Controls;
+using Avalonia.Interactivity;
 using Clock.Models;
 using Clock.ViewModels;
 
@@ -11,7 +12,27 @@ public partial class World : UserControl
     public World(WorldClockModel worldClockModel)
     {
         DataContext = new WorldClockViewModel(worldClockModel);
-        Unloaded += (_, _) => ((WorldClockViewModel)DataContext).Dispose();
+        SizeChanged += OnSizeChanged;
+        Unloaded += OnUnloaded;
         InitializeComponent();
+    }
+
+    private void OnSizeChanged(object? sender, SizeChangedEventArgs e)
+    {
+        var wide = e.NewSize.AspectRatio >= 1.5;
+        
+        Grid.ColumnDefinitions = new ColumnDefinitions(wide ? "*,*,*,*" : "*,*");
+        Grid.RowDefinitions = new RowDefinitions(wide ? "*" : "*,*");
+        Grid.SetColumn(Third, wide ? 2 : 0);
+        Grid.SetRow(Third, wide ? 0 : 1);
+        Grid.SetColumn(Fourth, wide ? 3 : 1);
+        Grid.SetRow(Fourth, wide ? 0 : 1);
+    }
+
+    private void OnUnloaded(object? sender, RoutedEventArgs e)
+    {
+        ((WorldClockViewModel)DataContext!).Dispose();
+        SizeChanged -= OnSizeChanged;
+        Unloaded -= OnUnloaded;
     }
 }
