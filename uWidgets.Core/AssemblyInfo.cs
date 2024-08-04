47d436da2c8ea180ba26eae4f800af07eca9bda0
creewick <ilia_y@mail.ru>
2024-08-04 09:37:05 +0300

    0.3.2

--- uWidgets.Core/AssemblyInfo.cs
+++ uWidgets.Core/AssemblyInfo.cs
@@ -3,4 +3,4 @@
 [assembly: AssemblyTitle("uWidgets.Core")]
 [assembly: AssemblyDescription("Core functionality for uWidgets")]
 [assembly: AssemblyCompany("creewick")]
-[assembly: AssemblyVersion("0.3.0")]
+[assembly: AssemblyVersion("0.3.2")]
